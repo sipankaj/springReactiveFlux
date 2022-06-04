58c127cec56df02f92932dd336b8f2a513e4f2d7
Pankaj Singh <pankajsingh.nitb@gmail.com>
2022-06-04 21:27:29 +0530

    first commit

---
+++ src/main/java/com/sipankaj/springReactiveFlux/SpringReactiveFluxApplication.java
@@ -0,0 +1,13 @@
+package com.sipankaj.springReactiveFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipankaj/springReactiveFlux/SpringReactiveFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipankaj.springReactiveFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
